48b38215fb0bbc9d94a70b02ebd1b38b4f68fa29
erinxclark <erinxclark@gmail.com>
2015-09-28 16:40:45 -0700

    first

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -1,94 +1,100 @@
 $(document).ready(function() {
 
 var counter = 0;
+var hasWinner = false;
 
+var field0 = '#field0'
+var field1 = '#field1'
+var field2 = '#field2'
+var field3 = '#field3'
+var field4 = '#field4'
+var field5 = '#field5'
+var field6 = '#field6'
+var field7 = '#field7'
+var field8 = '#field8'
 
-var a1 = $('#field0').attr('src','img/path');
-var a2 = $('#field1').attr('src','img/path');
-var a3 = $('#field2').attr('src','img/path');
-var b1 = $('#field3').attr('src','img/path');
-var b2 = $('#field4').attr('src','img/path');
-var b3 = $('#field5').attr('src','img/path');
-var c1 = $('#field6').attr('src','img/path');
-var c2 = $('#field7').attr('src','img/path');
-var c3 = $('#field8').attr('src','img/path');
-
-var row1= [
-  {cell1: a1},
-  {cell2: a2},
-  {cell3: a3}
-]
-var row2= [
-  {cell1: b1},
-  {cell2: b2},
-  {cell3: b3}
-]
-var row3= [
-  {cell1: c1},
-  {cell2: c2},
-  {cell3: c3}
-]
-var col1= [
-  {cell1: a1},
-  {cell2: b1},
-  {cell3: c1}
-]
-var col2= [
-  {cell1: a2},
-  {cell2: b2},
-  {cell3: c2}
-]
-var col3= [
-  {cell1: a3},
-  {cell2: b3},
-  {cell3: c3}
-]
-var dia1= [
-  {cell1: a1},
-  {cell2: b2},
-  {cell3: c3}
-]
-var dia2= [
-  {cell1: a3},
-  {cell2: b2},
-  {cell3: c1}
+var cellCheck = [
+  {x: 0, field: field0},
+  {x: 0, field: field1},
+  {x: 0, field: field2},
+  {x: 0, field: field3},
+  {x: 0, field: field4},
+  {x: 0, field: field5},
+  {x: 0, field: field6},
+  {x: 0, field: field7},
+  {x: 0, field: field8}
 ]
 
-var winCombos =
-  [row1, row2, row3]
-  [col1, col2, col3]
-  [dia1, dia2]
+var player1 = prompt("What is your name?","Player 1 Name");
+var player2 = prompt("What is your name?","Player 2 Name");
 
+$(".turn").append(player1);
 
 //each click counts counter++ until counter is even or odd. With booleans or %.
 //counter === 0 deliver RedCross, counter === 1 deliver knot.
 
 //begin working Check Boxes
 
-  $("td.cell").click(function()
+$("td.cell").click(function()
+{
+  if (counter % 2 === 0) {
+    $("<img class='cross' src='images/redcross.jpg'/>").appendTo($(this));
+    $(".turn").text(player2).toggle(true);
+    $(this).off();
+  }
+  else  {
+    $("<img class='knot' src='images/knot.jpg'/>").appendTo($(this));
+    $(".turn").text(player1).toggle(true);
+    $(this).off();
+  }
+
+  counter++;
+
+  cellCheck.forEach(function win(item, i) {
+    if (($(item.field).find('img').hasClass('cross'))) {
+      item.x = 1;
+    }
+    else if (($(item.field).find('img').hasClass('knot'))) {
+      item.x = -1;
+    };
+});
+
+var a1 = cellCheck[0].x;
+var a2 = cellCheck[1].x;
+var a3 = cellCheck[2].x;
+var b1 = cellCheck[3].x;
+var b2 = cellCheck[4].x;
+var b3 = cellCheck[5].x;
+var c1 = cellCheck[6].x;
+var c2 = cellCheck[7].x;
+var c3 = cellCheck[8].x;
+
+var winCombos= [
+  {cell1: a1, cell2: a2, cell3: a3},
+  {cell1: b1, cell2: b2, cell3: b3},
+  {cell1: c1, cell2: c2, cell3: c3},
+  {cell1: a1, cell2: b1, cell3: c1},
+  {cell1: a2, cell2: b2, cell3: c2},
+  {cell1: a3, cell2: b3, cell3: c3},
+  {cell1: a1, cell2: b2, cell3: c3},
+  {cell1: a3, cell2: b2, cell3: c1}
+];
+
+  winCombos.forEach(function (item, i) {
+    if ((item.cell1)+(item.cell2)+(item.cell3)===3)
     {
-      if (counter % 2 === 0) {
-        $("<img class='cross' src='images/redcross.jpg'/>").appendTo($(this));
-        $(this).off();
+        swal(player1+" Wins!");
+        hasWinner = true;
     }
-      else  {
-        $("<img class='knot' src='images/knot.jpg'/>").appendTo($(this));
-        $(this).off() };
-    counter++;
-
-    if (counter === 9) {
-      alert("Game ended!")
+    else if ((item.cell1)+(item.cell2)+(item.cell3)===-3)
+    {
+        swal(player2+" Wins!");
+        hasWinner = true;
     }
-
-//End working Check Boxes, kind of. Able to fill board with 9 alternating characters.
-
-  // function winner () {
-  //   if (img0 == img1 && img2)
-  //   {alert("Winner!")
-  //   }
-  //   };
-
-
-
   });
+    if ((counter === 9) && (hasWinner == false))
+    {
+        swal("Game ended!");
+    }
+});
 });
